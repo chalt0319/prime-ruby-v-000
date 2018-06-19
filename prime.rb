a4a51481df794e0274a150d3fb552328c471fa47
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-19 16:20:57 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,22 +1,14 @@
 def prime?(number)
-  
   if number < 2 
     return false 
   end 
-  
   prime_number = true 
   counter = 2 
-  
     while counter <= number / 2 
-    
       if number % counter == 0 
         prime_number = false 
       end 
-      
     counter += 1
-    
     end 
-    
   prime_number
-  
 end 
