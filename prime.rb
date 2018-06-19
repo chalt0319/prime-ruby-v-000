9859461067e4f9534ecf6ed0c487e463543081a9
Courtney Haltiwanger <chalt0319@gmail.com>
2018-06-19 16:25:19 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -5,7 +5,7 @@ def prime?(number)
   prime_number = true 
   counter = 2 
     while counter <= number / 2 
-      if number % counter == 0 
+      if number % counter == 0
         prime_number = false 
       end 
     counter += 1
